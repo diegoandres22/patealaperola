1032de0a351d0152b119c9b6219a4ba81fee92e3
diegoandres22 <diego.a.v3005@gmail.com>
2025-06-25 19:55:09 -0400

    FEAT: añado ruta dinamica de detalle de rifa

---
+++ src/component/1-screens/rafflesDetailScreen/rafflesDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RafflesDetail = () => {
+    return (
+        <div className='w-full min-h-[74vh] p-10 flex '>
+            detalle de la rifa ahora si
+        </div>
+    )
+}
