f729713c880c5d59177f63da8ce42e3120e6372b
diegoandres22 <diego.a.v3005@gmail.com>
2025-06-25 23:45:55 -0400

    FEAT: añado componente con detalle de la rifa vacío

--- src/component/1-screens/rafflesDetailScreen/rafflesDetail.tsx
+++ src/component/1-screens/rafflesDetailScreen/rafflesDetail.tsx
@@ -1,9 +1,10 @@
+import { RaffleDetailSection } from '@/component/2-sections/raffleDetailSection/raffleDetailSection'
 import React from 'react'
 
 export const RafflesDetail = () => {
     return (
-        <div className='w-full min-h-[74vh] p-10 flex '>
-            detalle de la rifa ahora si
+        <div className='w-full min-h-[74vh] pt-10 flex justify-center'>
+            <RaffleDetailSection />
         </div>
     )
 }
---
+++ src/component/2-sections/raffleDetailSection/raffleDetailSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RaffleDetailSection = () => {
+    return (
+        <div className="min-h-screen w-3/4 bg-neutral-200/20 rounded-xl">
+            seccion de detalle de la rifa
+        </div>
+    )
+}
