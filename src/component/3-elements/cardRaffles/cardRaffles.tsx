56687650982d08497f01f92efb6950628ba741dd
diegoandres22 <diego.a.v3005@gmail.com>
2025-06-29 23:21:01 -0400

    FIX: corrijo slint type errado

--- src/component/3-elements/cardRaffles/cardRaffles.tsx
+++ src/component/3-elements/cardRaffles/cardRaffles.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Card, CardFooter, CardHeader, Chip, Link } from '@heroui/react'
 import { Image } from "@heroui/image";
-import { CardRafflesProps } from '@/types';
+import { Raffle } from '@/types';
 
-export const CardRaffles: React.FC<CardRafflesProps> = ({ title, imageUrl, ticketPrice, minPurchase, raffleStatus }) => {
+export const CardRaffles: React.FC<Raffle> = ({ title, image, ticketPrice, minPurchase, raffleStatus }) => {
 
     let chipLabel = '';
     let chipColor: 'danger' | 'success' | 'warning' | 'secondary' | 'primary' | 'default' = 'danger';
@@ -35,7 +35,7 @@ export const CardRaffles: React.FC<CardRafflesProps> = ({ title, imageUrl, ticke
                     removeWrapper
                     alt="Card example background"
                     className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                    src={imageUrl}
+                    src={image}
                 />
                 <CardFooter className="absolute bg-white/20 bottom-0 border-zinc-100/30 z-10 justify-between items-center max-h-14 pt-0">
                     <div className='max-h-10 '>
@@ -50,39 +50,3 @@ export const CardRaffles: React.FC<CardRafflesProps> = ({ title, imageUrl, ticke
         </Link>
     )
 }
-
-// import React from 'react'
-// import { Card, CardFooter, CardHeader, Chip, Link } from '@heroui/react'
-// import { Image } from "@heroui/image";
-// import { CardRafflesProps } from '@/types';
-
-
-
-// export const CardRaffles: React.FC<CardRafflesProps> = ({ title, imageUrl, ticketPrice, minPurchase }) => {
-//     return (
-//         <Link href="/raffle" className='w-[300px] h-[300px]' color='secondary'>
-//             <Card isFooterBlurred className="w-[300px] h-[300px] col-span-12 sm:col-span-5 hover:scale-110 transition-transform
-//         duration-300 ease-in-out ">
-//                 <CardHeader className="absolute z-10 top-1 flex-col items-start">
-//                     <p className="text-lg text-white/80 uppercase font-extrabold ">Patea la perola y gana</p>
-//                     <h4 className="text-red-500 font-OFF font-black text-2xl ">{title}</h4>
-//                 </CardHeader>
-//                 <Image
-//                     removeWrapper
-//                     alt="Card example background"
-//                     className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-//                     src={imageUrl}
-//                 />
-//                 <CardFooter className="absolute bg-white/20 bottom-0 border-zinc-100/30 z-10 justify-between items-center max-h-14 pt-0">
-//                     <div className='max-h-10 '>
-//                         <p className="text-black font-bold text-lg max-h-6"> <strong className='text-xl'>{ticketPrice}$</strong> cada boleto</p>
-//                         <p className="text-black font-bold text-lg max-h-6">Compra mínima {minPurchase}</p>
-//                     </div>
-//                     <Chip color="danger" variant="dot" className="text-lg mt-4 text-black" radius="md" size="sm">
-//                         Finalizada
-//                     </Chip>
-//                 </CardFooter>
-//             </Card>
-//         </Link>
-//     )
-// }
