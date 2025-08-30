d92df90583d604c16a24f5852985e4d3975b8715
diegoandres22 <diego.a.v3005@gmail.com>
2025-08-30 18:21:33 -0400

    FIX: mejora la presentación de mensajes de carga y no encontrados en la validación de tickets

--- src/component/3-elements/logoImage.tsx
+++ src/component/3-elements/logoImage.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Image } from "@heroui/image";
 import { Link } from '@heroui/react';
-import isologo from '@/app/isologo.png'; 
+import isologo from '@/app/isologo.png';
 
 
 export const LogoImage = () => {
     return (
         <Link href="/" >
-            <Image
-                src={isologo.src} 
-                width={120}
-                height={80}
-                alt="Marca del sitio"
-            />
+            {isologo?.src && (
+                <Image
+                    src={isologo.src}
+                    width={120}
+                    height={80}
+                    alt="Marca del sitio"
+                />
+            )}
         </Link>
     )
 }
