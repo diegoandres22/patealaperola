40518e6779b650533630067ea8825f79b850ec3f
diegoandres22 <diego.a.v3005@gmail.com>
2025-09-02 22:11:41 -0400

    REFAC: elimina código comentado y reorganiza las interfaces de bancos y cuentas bancarias

--- src/types/banksAcountsProps.ts
+++ src/types/banksAcountsProps.ts
@@ -7,3 +7,21 @@ export interface BanksAcounts {
     Titular: string;
 
 }
+
+export interface BankAccount {
+    id: string;
+    pay_method: string;
+    holder_name_cta: string;
+    document_name: number | string;
+    number_cta_1: number | string;
+    number_cta_2: string;
+    email_cta: string;
+    is_active: boolean;
+}
+
+export interface BanksState {
+    banksList: BankAccount[];
+    selectedBank: BankAccount | null;
+    loading: boolean;
+    error: string | null;
+}
